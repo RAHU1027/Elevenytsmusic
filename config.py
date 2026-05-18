1f406c26b7a5a294aaf2a50e1d5ee00da8d96bf5
Artist <satyamshiv55@gmail.com>
2026-05-18 18:22:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -97,14 +97,14 @@ class Config:
         # URLs for various bot images
         self.DEFAULT_THUMB: str = getenv(
             "DEFAULT_THUMB",
-            "https://files.catbox.moe/6yi64z.png"  
+            "https://files.catbox.moe/nhg5ko.png"  
         )
         self.PING_IMG: str = getenv(
-            "PING_IMG", "https://files.catbox.moe/6yi64z.png")    
+            "PING_IMG", "https://files.catbox.moe/nhg5ko.png")    
         self.START_IMG: str = getenv(
-            "START_IMG", "https://files.catbox.moe/6yi64z.png")  
+            "START_IMG", "https://files.catbox.moe/nhg5ko.png")  
         self.RADIO_IMG: str = getenv(
-            "RADIO_IMG", "https://files.catbox.moe/6yi64z.png")    
+            "RADIO_IMG", "https://files.catbox.moe/nhg5ko.png")    
 
         # ============ MODERATION ============
         # List of usernames to exclude from admin mentions
